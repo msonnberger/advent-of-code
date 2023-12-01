53b352d95f9cda70033d94af02148418e58bfa0b
Martin Sonnberger <martin@sonnberger.email>
2023-12-01 06:32:01 +0100

    day 1

---
+++ 2023/01.py
@@ -0,0 +1,23 @@
+data = open("01.in").read()
+
+words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+def solve(part2=False):
+    total = 0
+    for line in data.split("\n"):
+        digits_only = ""
+        for i, ch in enumerate(line):
+            if part2:
+                for j, word in enumerate(words):
+                    if line[i:].startswith(word):
+                        digits_only += str(j + 1)
+                        
+            if ch.isdigit():
+                digits_only += ch
+
+        total += int(digits_only[0] + digits_only[-1])
+    
+    return total
+
+print(f"part 1: {solve()}")
+print(f"part 2: {solve(True)}")
