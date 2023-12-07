b350834a8c0084f663bb4534206527df332052ac
Martin Sonnberger <martin@sonnberger.email>
2023-12-07 11:09:12 +0100

    day 7 refactor

--- 2023/07.py
+++ 2023/07.py
@@ -2,42 +2,38 @@ from aocd import data
 from collections import Counter
 import functools
 
-hands = [line.split() for line in data.splitlines()]
-hands = [tuple([cards, int(bid)]) for cards, bid in hands]
+hands = [(hand, int(bid)) for hand, bid in (l.split() for l in data.splitlines())]
 
 
 def get_strength(count):
     distinct = len(count)
 
-    # no three of a kind --> two pair
-    if distinct == 3 and not any(c == 3 for c in count.values()):
-        return 2.5
-    # no four of a kind --> full house
-    elif distinct == 2 and not any(c == 4 for c in count.values()):
-        return 3.5
+    # handle two pair and full house (where number of distinct cards does not correspond to hand type)
+    if not any(c == 6 - distinct for c in count.values()):
+        distinct += 0.5
 
-    return 6 - distinct
+    return -distinct
 
 
 def get_count(cards, part2):
     count = Counter(cards)
 
-    if (not part2) or len(count) == 1 or not "J" in count:
+    if not part2 or len(count) == 1 or not "J" in count:
         return count
 
-    most_common = count.most_common(1)[0][0]
+    first, second = count.most_common(2)
 
-    if most_common == "J":
-        most_common = count.most_common(2)[1][0]
+    if first[0] == "J":
+        first = second
 
-    count[most_common] += count["J"]
+    count[first[0]] += count["J"]
     del count["J"]
 
     return count
 
 
 def cmp(a, b, part2=False):
-    a_count, b_count = get_count(a[0], part2), get_count(b[0], part2)
+    a_count, b_count = get_count(a, part2), get_count(b, part2)
     a_strength, b_strength = get_strength(a_count), get_strength(b_count)
 
     if a_strength != b_strength:
@@ -46,7 +42,7 @@ def cmp(a, b, part2=False):
     values = "23456789TJQKA" if not part2 else "J23456789TQKA"
 
     for i in range(5):
-        a_value, b_value = values.index(a[0][i]), values.index(b[0][i])
+        a_value, b_value = values.index(a[i]), values.index(b[i])
 
         if a_value != b_value:
             return a_value - b_value
@@ -54,10 +50,10 @@ def cmp(a, b, part2=False):
     return 0
 
 
-hands.sort(key=functools.cmp_to_key(lambda a, b: cmp(a, b, part2=False)))
+hands.sort(key=functools.cmp_to_key(lambda a, b: cmp(a[0], b[0], part2=False)))
 total = sum([hand[1] * (i + 1) for i, hand in enumerate(hands)])
 print(f"part 1: {total}")
 
-hands.sort(key=functools.cmp_to_key(lambda a, b: cmp(a, b, part2=True)))
+hands.sort(key=functools.cmp_to_key(lambda a, b: cmp(a[0], b[0], part2=True)))
 total = sum([hand[1] * (i + 1) for i, hand in enumerate(hands)])
 print(f"part 2: {total}")
